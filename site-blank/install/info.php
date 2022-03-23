4c179072a267bc1ea1357f2e57234365928b913a
yogeshborkargit <yogeshborkar@live.com>
2022-03-23 12:46:38 +0530

    Initial installation is complete
    Initial installation is complete with classic template

--- site-blank/install/info.php
+++
@@ -1,7 +0,0 @@
-<?php namespace ProcessWire;
-if(!defined("PROCESSWIRE_INSTALL")) die();
-$info = array(
-	'title' => "Blank", 
-	'summary' => "This profile includes only the bare minimum pages, fields and templates, giving you essentially a blank slate. ", 
-	'screenshot' => ""
-);
--- site-blank/templates/_init.php
+++
@@ -1,6 +0,0 @@
-<?php namespace ProcessWire;
-
-// Optional initialization file, called before rendering any template file.
-// This is defined by $config->appendTemplateFile in /site/config.php.
-// Use this to define shared variables, functions, classes, includes, etc. 
-
